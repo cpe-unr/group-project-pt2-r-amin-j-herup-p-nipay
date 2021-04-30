83dd1464a0f4259287c733290e93895c76959d5a
jherup1 <jherup@nevada.unr.edu>
2021-04-30 02:14:46 -0700

    finished echo.h

---
+++ echo.h
@@ -0,0 +1,24 @@
+#ifndef ECHO_H
+#define ECHO_H
+
+#include "processor.h"
+#include <iostream>
+
+class Echo: public Processor{
+	int delay;
+	int newDelay;
+	public:
+		Echo();
+		Echo(double newDelay);
+
+		double getDelay();
+		void setLevel(double newDelay);
+
+		void processorMonoE(int size, unsigned char* buffer);
+		void processorStereoE(int sizeR, int sizeL, unsigned char* bufferR, unsigned char* bufferL);
+		void processorMonoS(int size, short* buffer);
+		void processorStereoS(int sizeR, int sizeL, short* bufferR, short* bufferL);
+
+
+};
+#endif
