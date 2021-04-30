df6e6df124f8ae808241d6a8752f7cf4d10631a7
jherup1 <jherup@nevada.unr.edu>
2021-04-30 01:43:07 -0700

    edited getters and setters for noiseGate.h

--- noiseGate.h
+++ noiseGate.h
@@ -7,15 +7,17 @@
 class NoiseGate: public Processor{
 	double gateLevel;
 	public:
-		void processorMonoE(int size, unsigned char* buffer) = 0;
-		void processorStereoE(int sizeR, int sizeL, unsigned char* bufferR, unsigned char* bufferL) = 0;
-		void processorMonoS(int size, short* buffer) = 0;
-		void processorStereoS(int sizeR, int sizeL, short* bufferR, short* bufferL) = 0;
-
 		NoiseGate();
 		NoiseGate(double level);
 
 		double getLevel();
 		void setLevel(double level);
+
+		void processorMonoE(int size, unsigned char* buffer) = 0;
+		void processorStereoE(int sizeR, int sizeL, unsigned char* bufferR, unsigned char* bufferL) = 0;
+		void processorMonoS(int size, short* buffer) = 0;
+		void processorStereoS(int sizeR, int sizeL, short* bufferR, short* bufferL) = 0;
+
+
 };
 #endif
